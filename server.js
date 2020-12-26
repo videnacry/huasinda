0532e67c9930fc89270092bb1d12f904aeb9e79b
beron <bleink@outlook.es>
2020-12-26 01:42:11 +0100

    server.js - links route to get links

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ require('dotenv/config')
 const { join } = require('path')
 const express = require ('express')
 const app = express()
+const buildPath = join(process.cwd(), 'build')
 
-app.use(express.static(join(process.cwd(), '/build')))
+app.use(express.static(buildPath))
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile('./build/index.html')
+app.get('/links', async (req, res) => {
+    const { show } = require('./server/link')
+    const urls = ['http://localhost:3000/', 'http://localhost:3000/', 'http://localhost:3000/']
+    const links = []
+    for (let url of urls) {
+        const link = await show(url)
+        links.push(link)
+    }
+    res.status(200).json({links})
+})
+
+app.get('*', (req, res) => {
+    res.status(200).sendFile(join(buildPath,'index.html'))
 })
 
 app.listen(process.env.PORT, () => console.log('http://localhost:3000/'))
