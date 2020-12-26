7333384cf40bf6a258ef0b0c07aa439f91f322ad
beron <bleink@outlook.es>
2020-12-26 22:56:42 +0100

    server.js - changed urls to get links info

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ app.use(express.static(buildPath))
 
 app.get('/links', async (req, res) => {
     const { show } = require('./server/link')
-    const urls = ['http://localhost:3000/', 'http://localhost:3000/', 'http://localhost:3000/']
+    const urls = [
+        'https://www.ecojardinmagico.com/como-construir-pisos-de-tierra-paso-a-paso/',
+        'https://bioconstruccionfutura.com/congreso-2020/',
+        'https://bioconstruir.es/geometria-sagrada/',
+        'https://www.ecojardinmagico.com/7-ideas-para-crear-un-huerto-vertical-en-casa/'
+    ]
     const links = []
     for (let url of urls) {
         const link = await show(url)
