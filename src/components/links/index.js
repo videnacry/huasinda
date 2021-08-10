fc9bb24e333d54c8065a696c8da76e52dc907945
beron <bleink@outlook.es>
2021-08-10 23:40:44 +0200

    links component .- remove fetch, add  js with import of imgs, add responsiveness to show 3 links  in phones instead of 4, remove underline

--- src/components/links/index.js
+++ src/components/links/index.js
@@ -1,49 +1,16 @@
-import { useState, useEffect } from 'react'
 import Box from '../box'
 import './index.css'
+import links from './links'
 
 const Links = () => {
-
-    const [links, setLinks] = useState([])
-    const [pageLinks, setPageLinks] = useState([
-        {
-            url: 'https://www.ecojardinmagico.com/como-construir-pisos-de-tierra-paso-a-paso/',
-            img_path: 'https://www.ecojardinmagico.com/wp-content/uploads/2020/12/Piso-de-tierra-principal.jpg',
-            name: 'Cómo construir pisos de tierra paso a paso',
-            content: 'Los pisos de tierra son una gran opción para ...'
-        }, {
-            url: 'https://bioconstruccionfutura.com/congreso-2020/',
-            img_path: 'https://bioconstruccionfutura.com/wp-content/uploads/2018/07/Logo-Circular.png',
-            name: 'YA ESTAMOS EN LA SEMANA DEL CONGRESO CASAS SALUDABLES Y EFICIENTES',
-            content: 'Vas a aprender cómo tener una casa saludable ...'
-        }, {
-            url: 'https://bioconstruir.es/geometria-sagrada/',
-            img_path: 'https://bioconstruir.es/wp-content/uploads/2020/01/cropped-Bioconstruir-simbolo-rgb--32x32.png',
-            name: 'Geometría sagrada - Bioconstruir',
-            content: 'Es el conocimiento de las leyes de la medida,...'
-        }, {
-            url: 'https://www.ecojardinmagico.com/7-ideas-para-crear-un-huerto-vertical-en-casa/',
-            img_path: 'https://www.ecojardinmagico.com/wp-content/uploads/2020/12/Huerta-vertical.jpg',
-            name: '7 ideas para crear un huerto vertical en casa',
-            content: 'Sólo porque tengas un pequeño jardín o un peq...'
-        }
-    ])
-
-    useEffect(() => {
-        fetch('/links', {method: 'GET'}).then(res => res.json())
-        .then(({links}) => {
-            setLinks(links)
-            setPageLinks(links.slice(0, 4))
-        })
-    }, [])
-
+    const pageLinks = links.slice(0, 4)
     return(
         <div className="links-c">
-            <Box style={{width: '85%', height: '50%', borderRadius: '0', top: '0', right: '0'}}/>
+            <Box style={{width: '85%', height: '65%', borderRadius: '0', top: '0', right: '0'}}/>
             <h1>Links</h1>
             <ul className="links">
-                {pageLinks.map(link => (
-                    <li>
+                {pageLinks.map((link, idx) => (
+                    <li key={`link-${idx}`}>
                         <a className="link" href={link.url}>
                             <div className='img' style={{backgroundImage: 'url("' + link.img_path + '")'}}/>
                             <h3>{link.name[0].toUpperCase() + link.name.substr(1)}</h3>
---
+++ src/components/links/links.js
@@ -0,0 +1,30 @@
+import linkFirstSrc from './link-1.jpg'
+import linkSecondSrc from './link-2.jpg'
+import linkThirdSrc from './link-3.jpg'
+import linkFourthSrc from './link-4.jpg'
+
+const links = [
+    {
+        url: 'https://www.ecojardinmagico.com/como-construir-pisos-de-tierra-paso-a-paso/',
+        img_path: linkFirstSrc,
+        name: 'Cómo construir pisos de tierra paso a paso',
+        content: 'Los pisos de tierra son una gran opción para aquellos...'
+    }, {
+        url: 'https://bioconstruccionfutura.com/congreso-2020/',
+        img_path: linkSecondSrc,
+        name: '33 bioconstructores, arquitectos e investigadores que están llevando adelante un trabajo destacado en cada una de sus especialidades',
+        content: 'Si piensas construir o eres un profesional o técnico...'
+    }, {
+        url: 'https://www.fundacionaquae.org/consejos-para-cuidar-del-agua/',
+        img_path: linkThirdSrc,
+        name: 'Hacks para cuidar del agua',
+        content: 'El agua es el origen de la vida. Sin ella, no habría...'
+    }, {
+        url: 'https://www.ecojardinmagico.com/7-ideas-para-crear-un-huerto-vertical-en-casa/',
+        img_path: linkFourthSrc,
+        name: '7 ideas para crear un huerto vertical en casa',
+        content: 'Sólo porque tengas un pequeño jardín o un peque...'
+    }
+]
+
+export default links
