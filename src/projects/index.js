5988fb9402d967524e47df08c45bbbfc27fdc806
beron <bleink@outlook.es>
2021-08-12 07:09:58 +0200

    replace projects page per imgs with text by carrousels

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,13 +1,14 @@
 import React, {useEffect} from 'react'
 
 import Header from '../components/header'
-import Greeting from '../webinar/greeting'
-import Section from './section'
+import Project from './project'
 import Box from '../components/box'
-import firstProjectImgSrc from '../img/rectangle/complete_outside.jpg'
-import secondProjectImgSrc from '../about-us/jumbotron.jpg'
 import Footer from '../components/footer'
 
+import projectFirst from './project-first'
+import projectSecond from './project-second'
+
+import './index.css'
 const Projects = () => {
     
     useEffect(() => {
@@ -16,20 +17,14 @@ const Projects = () => {
 
     const boxStyle = {bottom:'6vw', left: '10%', maxWidth: '400px'}
     return(
-        <div className='webinar-p'>
+        <div className='projects-p'>
             <Header/>
-            <div className='header-space'></div>
-            <hr style={{margin: '0 20px'}}/>
-            <Greeting start='22/12/2020' end='28/12/2020' title='Proyectos Destacados' msg=''/>
-            <Section title='22-12-2020' link='' imgSrc={firstProjectImgSrc} />
-            <div style={{position: 'relative'}}>
-                <Box lines={['Vivienda con un área de  100m2  y 2m de altura ubicada en conocoto']} dark style={boxStyle}/>
+            <div className='title'>
+                <Box dark style={{width: '100%', height: '100%', borderRadius: 0}}/>
+                <h1>Proyectos Destacados</h1>
             </div>
-            <div style={{height: '120px'}}/>
-            <Section title='22-12-2021' link='' imgSrc={secondProjectImgSrc} />
-            <div style={{position: 'relative'}}>
-                <Box lines={['Hibernadero con un área de  20m2  y 3m de altura ubicada en carapungo']} dark style={boxStyle}/>
-            </div>
+            <Project project={projectFirst}/>
+            <Project left project={projectSecond}/>
             <Footer/>
         </div>
     )
---
+++ src/projects/project-second.js
@@ -0,0 +1,16 @@
+import photoFirstSrc from '../img/greenhouse/finish.jpg'
+import photoSecondSrc from '../img/greenhouse/new-structure.jpg'
+import photoThirdSrc from '../img/greenhouse/coating.jpg'
+import photoFourthSrc from '../img/greenhouse/columns.jpg'
+
+const project = {
+    title: 'Hibernadero',
+    photosSrc: [
+        photoFirstSrc,
+        photoSecondSrc,
+        photoThirdSrc,
+        photoFourthSrc
+    ]
+}
+
+export default project
