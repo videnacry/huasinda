e56cca1b2b8411c8000577bead8fa1f61d815d39
beron <bleink@outlook.es>
2021-08-22 14:26:05 +0200

    summary.[js, css] .- put boxes inside divs to change position and resize with aspect-ratio

--- src/webinar/summary.js
+++ src/webinar/summary.js
@@ -12,17 +12,25 @@ const Summary = (props) => {
             <div className='frame'>
                 {props.boxes.dark ? 
                 <>
-                <Box absolute style={{width: '54%', height: '36%', top: '-42%', right: '0'}}/>
-                <Box absolute wrap radius dark shadowTopLeft style={{maxWidth: '50%', minHeight: '50%', width: '45%', top: '-35%', right: '1%'}} lines={props.boxes.dark.elmts}/> 
+                <div className='box-light'>
+                    <Box style={{width: '100%', height: '100%'}}/>
+                </div>
+                <div className='box-dark'>
+                    <Box wrap radius dark shadowTopLeft style={{width: '100%', height: '100%'}} lines={props.boxes.dark.elmts}/> 
+                </div>
                 </> : ''}
                 <div className='img' style={{backgroundImage: 'url(' + props.img + ')'}}/>
                 {props.boxes.light ? 
                 <>
-                <Box absolute dark style={{width: '100%', bottom: '-15%', left: '0'}}/>
-                <Box absolute wrap radius shadowBottomRight style={{width: '70%', top: '70%', left: '1%', backgroundColor: 'white'}} lines={props.boxes.light.elmts}/>
+                <div className='line-dark'>
+                    <Box dark style={{width: '100%', height: '100%'}}/>
+                </div>
+                <div className='box-white'>
+                    <Box wrap radius shadowBottomRight style={{width: '100%', height: '100%', backgroundColor: 'white'}} lines={props.boxes.light.elmts}/>
+                </div>
                 </> : ''}
             </div>
-            {props.boxes.light ? <Box style={{width: '70%', marginBottom: '-36%', opacity: 0}} lines={props.boxes.light.elmts}/> : ''}
+            {props.boxes.light ? <Box wrap style={{width: '70%', marginBottom: '-20%', opacity: 0}} lines={props.boxes.light.elmts}/> : ''}
         </section>
     )
 }
