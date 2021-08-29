ed74e27786412106d7c0118b8cef06dee8dec293
beron <bleink@outlook.es>
2021-08-29 10:17:53 +0200

    change module-modal html and css, so it shows part of an image as background instead of black box and has two gray boxes as background for the modules cards

--- src/webinar/price/modal-modules/index.js
+++ src/webinar/price/modal-modules/index.js
@@ -1,25 +1,41 @@
 import Box from '../../../components/box'
 import Module from './module'
 import './index.css'
+import ImgSrcEcovillage from './ecovillage.jpg'
 const ModulesModal = () => {
     return(
         <div className='modal-s'>
             <div className='backdrop'/>
             <div className='modal'>
-                <div className='column-dark'>
-                    <Box dark style={{width: '95%', height: '97.4%', top: '1.2%', left: '0'}}/>
+                <div className='background' style={{backgroundImage: `url(${ImgSrcEcovillage})`}}/>
+                <div className='box-first-white'>
+                    <Box fill style={{backgroundColor: 'white'}}/>
+                </div>
+                <div className='box-second-white'>
+                    <Box fill style={{backgroundColor: 'white'}}/>
+                </div>
+                <div className='box-third-white'>
+                    <Box fill style={{backgroundColor: 'white'}}/>
                 </div>
                 <div className='header'>
                     <h1>**Módulos**</h1>
                     <button className='btn-close'><h1>x</h1></button>
                 </div>
-                <div className='container-gray'>
-                    <Module/>
-                    <Module/>
-                    <Module/>
-                    <Module/>
-                    <Module/>
-                    <Module/>
+                <div className='box-first-gray'>
+                    <Box fill/>
+                </div>
+                <div className='box-second-gray'>
+                    <Box fill/>
+                </div>
+                <div className='container'>
+                    <div className='modules'>
+                        <Module/>
+                        <Module/>
+                        <Module/>
+                        <Module/>
+                        <Module/>
+                        <Module/>
+                    </div>
                 </div>
             </div>
         </div>
