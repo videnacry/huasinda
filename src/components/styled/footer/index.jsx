f5fe05a74c70e593dfad16eb100ee4ac44350ecf
berón <bleink@outlook.es>
2023-09-07 20:41:42 +0200

    change footer structure

---
+++ src/components/styled/footer/index.jsx
@@ -0,0 +1,25 @@
+import Box from '../box'
+import './index.css'
+
+const Footer = () => {
+
+    return(
+        <div className="footer-c">
+            <Box absolute style={{width: '100%', height: '100%'}}/>
+            <div className='spheres'>
+                <div className="frame-brown">   
+                    <Box dark fill/>
+                </div>
+                <a href='mailto:bioedifica@gmail.com'>bioedifica@gmail.com</a>
+                <div className='sphere-small' />
+                <div className='sphere-big' />
+                <div className='sphere-small' />
+                <a href='tel:+593 96 138 5672'>TEL: +593 96 138 5672</a>
+            </div>
+            <div className='contact'>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
